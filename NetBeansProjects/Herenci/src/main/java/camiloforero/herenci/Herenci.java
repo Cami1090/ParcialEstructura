4704de2b4ee111eb510210aee6a7c627e53ba045
Cami <Cami@172.29.0.188>
2024-07-08 17:32:02 -0500

    Borrar alumnos

---
+++ NetBeansProjects/Herenci/src/main/java/camiloforero/herenci/Herenci.java
@@ -0,0 +1,16 @@
+/*
+ * 
+ */
+
+package camiloforero.herenci;
+
+/**
+ *
+ * @author Cami
+ */
+public class Herenci {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
